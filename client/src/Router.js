645551c7a08b25c74c49746170b0c4749a76d2d4
SmitaSriyaSarangi <sdvmcsc@gmail.com>
2024-10-11 01:00:51 +0530

    dashboard added

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
-
+import Dashboard from './components/Dashboard';
 
 function AppRouter() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Login />} />
-              
+                <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
         </Router>
     );
